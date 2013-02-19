028d35eb4a0763e0c76f6dfc745b99dec2716e29
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2013-02-19 03:23:04 +0000

    *Update Hercules 1093763fb0, 55134e898b, bf760554a5

--- src/map/status.c
+++ src/map/status.c
@@ -4750,7 +4750,7 @@ static unsigned short status_calc_matk(struct block_list *bl, struct status_chan
 	if (sc->data[SC_ZANGETSU])
 		matk += sc->data[SC_ZANGETSU]->val3;
 #endif
-    if (sc->data[SC_MAGICPOWER])
+    if (sc->data[SC_MAGICPOWER] && sc->data[SC_MAGICPOWER]->val4)
         matk += matk * sc->data[SC_MAGICPOWER]->val3 / 100;
     if (sc->data[SC_MINDBREAKER])
         matk += matk * sc->data[SC_MINDBREAKER]->val2 / 100;
@@ -10883,9 +10883,10 @@ int status_change_timer_sub(struct block_list* bl, va_list ap)
 		if (battle_check_target( src, bl, BCT_ENEMY ) > 0 &&
 			status_check_skilluse(src, bl, WZ_SIGHTBLASTER, 2))
 		{
-			skill_attack(BF_MAGIC,src,src,bl,WZ_SIGHTBLASTER,1,tick,0);
-			if (sce && !(bl->type&BL_SKILL)) //The hit is not counted if it's against a trap
+			if (sce && !(bl->type&BL_SKILL) //The hit is not counted if it's against a trap
+				&& skill_attack(BF_MAGIC,src,src,bl,WZ_SIGHTBLASTER,1,tick,0)){
 				sce->val2 = 0; //This signals it to end.
+			}
 		}
 		break;
 	case SC_CLOSECONFINE:
