2bf7dfef16ecb72976ba731fecfc6a9ab36fdeeb
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2013-03-08 14:12:32 +0000

    *Implement missing updates in rAthena 17161

--- src/map/map.c
+++ src/map/map.c
@@ -106,8 +106,8 @@ static int block_free_count = 0, block_free_lock = 0;
 static struct block_list *bl_list[BL_LIST_MAX];
 static int bl_list_count = 0;
 
-#define MAX_MSG 1500
-static char* msg_table[MAX_MSG]; // map Server messages
+#define MAP_MAX_MSG 1500
+static char* msg_table[MAP_MAX_MSG]; // map Server messages
 
 struct map_data map[MAX_MAP_PER_SERVER];
 int map_num = 0;
