c5d75eed033b6400ca2c230ade55be10c01da485
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2013-05-03 09:55:11 +0000

    *Update Hercules 6bca43acc31, 64adafab4a, c6f8d9ed12

--- src/map/pc.c
+++ src/map/pc.c
@@ -4014,6 +4014,7 @@ int pc_isUseitem(struct map_session_data *sd,int n)
 		return 0;
 
 	if( (item->item_usage.flag&NOUSE_SITTING) && (pc_issit(sd) == 1) && (pc_get_group_level(sd) < item->item_usage.override) ) {
+		clif_colormes(sd->fd,COLOR_WHITE,msg_txt(1477));
 		return 0; // You cannot use this item while sitting.
 	}
 
