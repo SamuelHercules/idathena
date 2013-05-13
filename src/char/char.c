edfe85411a5b4af094b5b8be245651aeed8c5b2a
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2013-05-13 02:52:38 +0000

    *Update rAthena up to 17327

--- src/char/char.c
+++ src/char/char.c
@@ -3398,24 +3398,18 @@ int parse_frommap(int fd)
 					int sfd;/* stat server fd */
 					RFIFOSKIP(fd, 2);/* we skip first 2 bytes which are the 0x3008, so we end up with a buffer equal to the one we send */
 
-					if( (sfd = make_connection(host2ip("stats.rathena.org"),(uint16)25421,true) ) == -1 ) {
+					if( (sfd = make_connection(host2ip("stats.rathena.org"),(uint16)25421,true,10) ) == -1 ) {
 						RFIFOSKIP(fd, RFIFOW(fd,2) );/* skip this packet */
 						break;/* connection not possible, we drop the report */
 					}
 
 					session[sfd]->flag.server = 1;/* to ensure we won't drop our own packet */
-
-					WFIFOHEAD(sfd, RFIFOW(fd,2) );
-
+					WFIFOHEAD(sfd, RFIFOW(fd,2));
 					memcpy((char*)WFIFOP(sfd,0), (char*)RFIFOP(fd, 0), RFIFOW(fd,2));
-
-					WFIFOSET(sfd, RFIFOW(fd,2) );
-
+					WFIFOSET(sfd, RFIFOW(fd,2));
 					flush_fifo(sfd);
-
 					do_close(sfd);
-
-					RFIFOSKIP(fd, RFIFOW(fd,2) );/* skip this packet */
+					RFIFOSKIP(fd, RFIFOW(fd,2));/* skip this packet */
 			}
 			break;
 
@@ -4469,7 +4463,7 @@ int check_connect_login_server(int tid, unsigned int tick, int id, intptr_t data
 		return 0;
 
 	ShowInfo("Attempt to connect to login-server...\n");
-	login_fd = make_connection(login_ip, login_port, false);
+	login_fd = make_connection(login_ip, login_port, false, 10);
 	if (login_fd == -1) { //Try again later. [Skotlex]
 		login_fd = 0;
 		return 0;
