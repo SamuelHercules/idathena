1b22db6c3cdb334f79b5e16a618cde73f245c4f5
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2013-07-04 08:58:08 +0000

    *Follow up r841

--- trunk/src/map/skill.c
+++ trunk/src/map/skill.c
@@ -5481,7 +5481,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 					if( index >= 0 && dstsd->inventory_data[index] && dstsd->inventory_data[index]->type == IT_WEAPON )
 					bonus = (8 + 2 * skill_lv) * dstsd->inventory_data[index]->wlv;
 				}
-				bonus += 5 * sd ? (pc_checkskill(sd, SA_FLAMELAUNCHER) + pc_checkskill(sd, SA_FROSTWEAPON) + pc_checkskill(sd, SA_LIGHTNINGLOADER) + pc_checkskill(sd, SA_SEISMICWEAPON)) : 1;
+				bonus += 5 * (sd ? (pc_checkskill(sd, SA_FLAMELAUNCHER) + pc_checkskill(sd, SA_FROSTWEAPON) + pc_checkskill(sd, SA_LIGHTNINGLOADER) + pc_checkskill(sd, SA_SEISMICWEAPON)) : 1);
 				clif_skill_nodamage( src, bl, skill_id, skill_lv, sc_start2(src, bl, type, 100, skill_lv, bonus, skill_get_time(skill_id,skill_lv)) );
 			} else if ( sd )
 				clif_skill_fail(sd,skill_id,USESKILL_FAIL_TOTARGET,0);
