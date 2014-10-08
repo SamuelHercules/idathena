1ae181e5b33a74b9021191397cac8a2f0a0614a2
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2014-10-08 00:14:39 +0000

    *Follow up r1547

--- src/map/path.h
+++ src/map/path.h
@@ -52,7 +52,7 @@ bool path_search_long(struct shootpath_data *spd,int16 m,int16 x0,int16 y0,int16
 // distance related functions
 bool check_distance(int dx, int dy, int distance);
 unsigned int distance(int dx, int dy);
-int check_distance_client(int dx, int dy, int distance);
+bool check_distance_client(int dx, int dy, int distance);
 int distance_client(int dx, int dy);
 
 #endif /* _PATH_H_ */
