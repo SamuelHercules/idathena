fc11da2aa4df76bb7f390d4371cd05a0441f7511
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2014-10-23 03:44:57 +0000

    *Update some of monsters database. *Follow up r1596

--- trunk/src/map/status.c
+++ trunk/src/map/status.c
@@ -9646,19 +9646,18 @@ int status_change_start(struct block_list* src,struct block_list* bl,enum sc_typ
 					val2 = val1 * 5; //Custom
 				break;
 			default:
-				switch( type ) {
-					case SC_KSPROTECTED:
-					case SC_XMAS:
-					case SC_SUMMER:
-					case SC_HANBOK:
-					case SC_OKTOBERFEST:
-						break; //Avoid the warning, because this status has no skill associated and all values already store in it
-					default:
-						if( calc_flag == SCB_NONE && StatusIconChangeTable[type] == SI_BLANK && StatusSkillChangeTable[type] == 0 ) {
+				if( calc_flag == SCB_NONE && StatusIconChangeTable[type] == SI_BLANK && StatusSkillChangeTable[type] == 0 ) {
+					switch( type ) {
+						case SC_KSPROTECTED:
+						case SC_XMAS:
+						case SC_SUMMER:
+						case SC_HANBOK:
+						case SC_OKTOBERFEST:
+							break; //Avoid the warning, because this status has no skill associated and all values already store in it
+						default:
 							ShowError("Unknown Status Change [%d]\n",type);
 							return 0; //Status change with no calc, no icon, and no skill associated?
-						}
-						break;
+					}
 				}
 				break;
 		}
