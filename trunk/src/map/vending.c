32977fd109cc0026d68d633e74f546c9cfe7a486
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2015-06-09 09:10:53 +0000

    *Follow up r1935

--- trunk/src/map/vending.c
+++ trunk/src/map/vending.c
@@ -320,7 +320,7 @@ int8 vending_openvending(struct map_session_data *sd, const char *message, const
 		sd->vending[i].value = min(value, (unsigned int)battle_config.vending_max_value);
 
 		//Player just moved item to cart and we don't have the correct cart ID yet
-		if( sd->status.cart[sd->vending[i].index].id == 0 ) {
+		if( battle_config.feature_autotrade && sd->status.cart[sd->vending[i].index].id == 0 ) {
 			struct item_data *idb = itemdb_search(sd->status.cart[index].nameid);
 			char msg[256];
 
