0b9ece77fb5b370363d599c90e6ad5e71599614a
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2015-08-17 00:17:01 +0000

    *Follow up r2002 *Update Hercules c215c9c21a

--- src/char/char.c
+++ src/char/char.c
@@ -1885,7 +1885,7 @@ int mmo_char_tobuf(uint8 *buffer, struct mmo_charstatus *p)
 	WBUFW(buf,54) = p->hair;
 
 	//When the weapon is sent and your option is riding, the client crashes on login!?
-	WBUFW(buf,56) = p->option&(0x20|0x80000|0x100000|0x200000|0x400000|0x800000|0x1000000|0x2000000|0x4000000|0x8000000) ? 0 : p->weapon;
+	WBUFW(buf,56) = (p->option&(0x20|0x80000|0x100000|0x200000|0x400000|0x800000|0x1000000|0x2000000|0x4000000|0x8000000) ? 0 : p->weapon);
 
 	WBUFW(buf,58) = p->base_level;
 	WBUFW(buf,60) = min(p->skill_point,INT16_MAX);
